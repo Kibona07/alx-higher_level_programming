e9be6fa1bf841120284122ba5577edb9bc5aa161
kibona07 <imanikibona07@gmail.com>
2024-03-12 05:36:57 +0300

    modified task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
-const y = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 const message = 'Missing size';
-if (isNaN(y)) {
+if (isNaN(x)) {
   console.log(message);
 } else {
-  for (let j = 0; j < y; j++) {
-    console.log('y'.repeat(y));
+  for (let j = 0; j < x; j++) {
+    console.log('x'.repeat(x));
   }
 }
